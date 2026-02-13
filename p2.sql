65a03bd434a7ea69f962f82540a041e7e24802ee
Siddharth Shukla <siddharth.shukla@bounteous.com>
2026-02-13 11:47:53 +0530

    15 practice questions done

--- p2.sql
+++ p2.sql
@@ -15,13 +15,24 @@ insert into department (department_id, department_name) values
 
 insert into address (address_id, street_address,city,state,postal_code) values
 (1,'123 Elm St','Springfield','IL',62701),
-(2,'456 Oak St','Springfield','IL',62521),
-(3,'789 Pine St','Springfield','IL',61820),
-(4,'102 Birch Rd','Springfield','IL',61602),
-(5,'205 Cedar Ave','Springfield','IL',62701),
-(6,'310 Maple Dr','Springfield','IL',62701),
-(7,'415 Oak Blvd','Springfield','IL',618),
-(8,'520 Pine Rd','Springfield','IL',62901);
+(2,'456 Oak St','Decatur','IL',62521),
+(3,'789 Pine St','Champaign','IL',61820),
+(4,'102 Birch Rd','Peoria','IL',61602),
+(5,'205 Cedar Ave','Chicago','IL',62701),
+(6,'310 Maple Dr','Urbana','IL',62701),
+(7,'415 Oak Blvd','Champaign','IL',618),
+(8,'520 Pine Rd','Carbondale','IL',62901);
+
+
+insert into student (student_id,first_name,last_name,birthdate,department_id,address_id) values
+(1, 'John','Doe','1995-04-15',1, 1),
+(2, 'Jane','Smith','1996-07-22',2, 2),
+(3, 'Alice','Johnson','1994-11-30',3, 3),
+(4, 'Michael','Brown', '1997-02-19',4, 4),
+(5, 'Sophia','Davis','1998-01-05',5, 5),
+(6, 'Daniel','Wilson','1995-06-10',6, 6),
+(7, 'Olivia', 'Martinez','1997-11-25',7, 1),
+(8, 'Ethan','Miller','1996-03-30',8, 2);
 
 
 select * from student;
@@ -34,4 +45,36 @@ select count(*) as total from student;
 -- department of john
 select d.department_name from department d join student s on s.department_id=d.department_id where s.first_name='John';
 
--- 
+-- List All Departments with Their Number of Students (Including Departments with No Students)
+select d.department_name, count(s.first_name) from department d left join student s on s.department_id=d.department_id group by d.department_name;
+
+--  Select all students with their department and address.
+select s.first_name,s.last_name, d.department_name, a.street_address from department d join student s on s.department_id=d.department_id join address a on a.address_id=s.address_id;
+
+-- students in cs department
+select s.first_name, s.last_name from student s join department d on s.department_id=d.department_id where d.department_name='Computer Science';
+
+-- students in spring field
+select s.first_name from student s join address a on s.address_id=a.address_id where a.city='Springfield';
+
+-- birthday in february
+select s.first_name, s.last_name from student s where extract(month from birth_date)=2; 
+
+-- update jane city name to new york
+update address set city='New York' where address_id=(select address_id from student where first_name = 'Jane');
+
+-- delete a student from student 
+delete from student where student_id=3;
+
+-- delete student from mechanical engineering department
+delete from student s using department d where s.department_id=d.department_id and d.department_id=2;
+
+-- how many students in each department
+select count(s.first_name) as total, d.department_name from student s left join department d on s.department_id=d.department_id group by d.department_name;
+
+-- Select all students with their department and address in New York. 
+select s.first_name,s.last_name, d.department_name, a.street_address from department d join student s on s.department_id=d.department_id join address a on a.address_id=s.address_id where a.city='New York';
+
+--  Find the name of the student who lives at '520 Pine Rd'
+select s.first_name,s.last_name from student s join address a on s.address_id=a.address_id where a.street_address='520 Pine Rd';
+
